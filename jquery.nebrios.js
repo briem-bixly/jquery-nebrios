0e3790128891040ac3b7bc0100e0cafc85ca122a
Brie Marvin <briem@bixly.com>
2015-08-28 08:20:06 -0700

    updated api request

--- jquery.nebrios.js
+++ jquery.nebrios.js
@@ -1,45 +1,51 @@
-(function() {
-    var nebriosFactory = function(jQuery) {
+(function($) {
+    var NebriOSClient;
+    NebriOSClient = (function() {
         var instance_name = '';
-        var $ = jQuery || window.jQuery;
-        var NebriOSClient = function (instance_name){
-            var NebriOSClient = this;
-            NebriOSClient.instance = instance_name;
-            NebriOSClient.api_request = function(api_module, view_name, method, payload) {
-                var url = 'https://' + this.instance + '.nebrios.com/api/v1/' + api_module + '/' + view_name;
-                if (method == "GET"){
-                    var params = $.param(payload);
-                    console.log(params);
-                    $.ajax({
-                        type: method,
-                        url: url + '?' + params,
-                        success: function(data) {
-                            this.returnData(data);
-                        }
-                    });
-                } else if (method == "POST" || method == "PUT") {
-                    $.ajax({
-                        type: method,
-                        url: url,
-                        data: payload,
-                        success: function(data) {
-                            this.returnData(data);
-                        }
-                    });
-                } else {
-                    $.ajax({
-                        type: method,
-                        url: url,
-                        success: function(data) {
-                            this.returnData(data);
-                        }
-                    });
-                }
-            };
-            NebriOSClient.returnData = function(data) {
-                console.log(data);
-            };
+        function NebriOSClient(instance_name){
+            this.instance = instance_name;
         }
+        NebriOSClient.prototype.api_request = function(api_module, view_name, method, payload, callback) {
+            var me = this;
+            me.callback = callback;
+            var url = 'https://' + this.instance + '.nebrios.com/api/v1/' + api_module + '/' + view_name;
+            if (method == "GET"){
+                var params = $.param(payload);
+                $.ajax({
+                    type: method,
+                    url: url + '?' + params,
+                    success: function(data) {
+                        me.returnData(data);
+                    }
+                });
+            } else if (method == "POST" || method == "PUT") {
+                $.ajax({
+                    type: method,
+                    url: url,
+                    data: payload,
+                    success: function(data) {
+                        me.returnData(data);
+                    }
+                });
+            } else {
+                $.ajax({
+                    type: method,
+                    url: url,
+                    success: function(data) {
+                        me.returnData(data);
+                    }
+                });
+            }
+        };
+        NebriOSClient.prototype.returnData = function(data) {
+            return (this.callback)(data);
+        };
         return NebriOSClient;
+    })();
+    $.nebriosclient = function(instance_name) {
+        if (!this.nebriosClientInstance) {
+            this.nebriosClientInstance = new NebriOSClient(instance_name || 'francois');
+        }
+        return this.nebriosClientInstance;
     };
-}());
+})(jQuery);
